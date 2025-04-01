71eb75cfdb084bbb2a29ab4497c7e7434e8230dd
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-04-01 11:48:10 +0530

    321

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Gallery from './components/Gallery';
 import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 
@@ -16,7 +14,6 @@ function App() {
         <Skills />
         <Projects />
         <Certifications />
-        <Gallery />
         <Contact />
       </main>
     </div>
